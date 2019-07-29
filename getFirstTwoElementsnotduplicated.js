2bb2b076b3d2d3091bbe66363b4e0df3b1c43454
Giuseppe Filomena <salsx@hotmail.it>
2019-07-29 14:34:16 +0200

    Update getFirstTwoElementsnotduplicated.js

--- getFirstTwoElementsnotduplicated.js
+++ getFirstTwoElementsnotduplicated.js
@@ -1,12 +1,11 @@
 var list = [5,23,23,66,5,23,7,0];
 
-
 function getFirstTwoElementsnotduplicated(array) {
-    let result = [];
-    for (var i = 0; i < array.length; i++) {
-        var item = array[i];
-        var count = 0;
-        for (var j = 0; j < array.length; j++) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i];
+        let count = 0;
+        for (let j = 0; j < array.length; j++) {
             if(item == array[j]) count++;
         }
         if(count == 1){
@@ -20,5 +19,5 @@ function getFirstTwoElementsnotduplicated(array) {
 }
 
 
-  var r =  getFirstTwoElementsnotduplicated(list);
-  console.log(r)
+var r =  getFirstTwoElementsnotduplicated(list);
+console.log(r)
